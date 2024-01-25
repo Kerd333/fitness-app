94c57fd1fcb90c11aa057669eb431c342a5e1c1a
Kerd333 <333kevinruiz@gmail.com>
2024-01-25 10:36:15 -0400

    made id and date optional

--- src/domain/entities/session.entity.ts
+++ src/domain/entities/session.entity.ts
@@ -2,9 +2,9 @@ import { ExerciseEntity } from "./exercise.entity";
 
 export class SessionEntity {
     constructor (
-        public id: number,
         public category: string,
         public exercises: ExerciseEntity[],
-        public date: Date,
+        public date?: Date,
+        public id?: number,
     ){}
 }
